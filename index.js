2f6ad5728a8d8994c8221d90671357ebf8aba297
Senal Perera <senalperera777@gmail.com>
2026-01-24 17:29:54 +0530

    Added function logic
    - added calculation parts for day duration

--- index.js
+++ index.js
@@ -10,13 +10,26 @@ submitBtn.addEventListener("click", ()=> {
     const date = dateInput.value
 
     if(name && date){
+        const duration = calculateDuration(date)
         warningText.style.display = "none"
+
         nameUlEl.innerHTML += `<li>${name}</li>`
-        daysUlEl.innerHTML += `<li>${date}</li>`
-        console.log("Entry logged: ", name, date)
+        daysUlEl.innerHTML += `<li>${duration}_Days</li>`
+        console.log("Entry logged: ", name, duration)
+
         nameInput.value = ""
         dateInput.value = ""
     } else {
         warningText.style.display = "block"
     }
 })
+
+function calculateDuration(date){
+    const today = new Date()
+    const oldDate = new Date(date)
+    // Milliseconds per day
+    const oneDay = 24 * 60 * 60 * 1000
+    // Getting the millisecond count since Jan 1, 1970 from each date -> subtract -> divide to get day count
+    const duration = Math.round((today.getTime() - oldDate.getTime())/oneDay)
+    return duration
+}
