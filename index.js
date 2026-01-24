6d5fc4afe58c36419eacbe7459e1f92961752385
Senal Perera <senalperera777@gmail.com>
2026-01-24 17:15:54 +0530

    Added input and button logic
    - user can add entries to the table using name and date

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-const searchBar = document.getElementById("search-bar")
+const nameInput = document.getElementById("name-input")
+const dateInput = document.getElementById("date-input")
+const submitBtn = document.getElementById("submit-button")
+const warningText = document.getElementById("warning")
+const nameUlEl = document.getElementById("name-li")
+const daysUlEl = document.getElementById("days-li")
 
-searchBar.addEventListener("keydown", (event)=> {
-    if(event.key == "Enter"){
-        const value = searchBar.value
-        console.log(value)
+submitBtn.addEventListener("click", ()=> {
+    const name = nameInput.value
+    const date = dateInput.value
+
+    if(name && date){
+        warningText.style.display = "none"
+        nameUlEl.innerHTML += `<li>${name}</li>`
+        daysUlEl.innerHTML += `<li>${date}</li>`
+        console.log("Entry logged: ", name, date)
+        nameInput.value = ""
+        dateInput.value = ""
+    } else {
+        warningText.style.display = "block"
     }
 })
